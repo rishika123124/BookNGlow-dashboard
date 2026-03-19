a2b2feb73f6e2f8d8940a30db6f86eb1d621d265
rishika123124 <rishikasoni2507@gamil.com>
2026-03-20 00:04:44 +0530

    Fix next.config.js - remove deprecated swcMinify option

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     optimizeCss: true,
     optimizePackageImports: ['lucide-react', 'react-hot-toast'],
